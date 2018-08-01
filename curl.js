b840d8f452bcde2b38ff2e6a17cb1809996c058b
Javier Carey <javier3@gmail.com>
2018-07-31 22:39:14 -0700

    updated curl, spacing the output to make readable, to properly print response status codes, etc...

--- curl.js
+++ curl.js
@@ -1,12 +1,13 @@
 const request = require('request');
 
 module.exports = (urlX) => request(urlX, (err, response, body) => {
+  console.log(urlX);
   if (err) {
     throw err;
   } else {
-    process.stdout.write('error: ' + err); // Print the error if one occurred
-    process.stdout.write('statusCode: ' + response && response.statusCode); // Print the response status code if a response was received
-    process.stdout.write('body: ' + body); // Print the HTML for the Google homepage
+    process.stdout.write('error: ' + err + '\n'); // Print the error if one occurred
+    process.stdout.write('statusCode: ' + JSON.stringify(response) + ' - ' + response.statusCode + '\n'); // Print the response status code if a response was received
+    process.stdout.write('body: \n' + body ); // Print the HTML for the Google homepage
     process.stdout.write('\nprompt > ');
   }
 });
